72fd1dfd5ecadb1f5337780ee710d1654b2eb10e
Pengcheng Zhang <pengchengz30@gmail.com>
2024-12-02 12:09:23 -0500

    add html

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Boost_Searcher
+//
+//  Created by Pengcheng Zhang on 12/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
